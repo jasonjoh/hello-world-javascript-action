021bd17df47cbff7fa0972911a0fcb1f819b45a9
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-06-08 13:27:06 -0400

    Testing

---
+++ index.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as Webhooks from '@octokit/webhooks';
+
+async() => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+
+    if (github.context.eventName === 'pull_request') {
+      const pullPayload = github.context.payload as Webhooks.Webhooks.WebhookPayloadPullRequest;
+
+      const octokit = github.getOctokit('');
+      const files = await octokit.pulls.listFiles({
+        owner: 'jasonjoh',
+        repo: 'hello-world-javascript-action',
+        pull_number: pullPayload.pull_request.number,
+        per_page: 100
+      });
+
+      files.data.forEach((file) => {
+        console.log(`File: ${file.filename}`);
+      });
+    }
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
